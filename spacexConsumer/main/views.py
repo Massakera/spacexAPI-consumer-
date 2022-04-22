cc6d1b0c19e67495711e8f28cf65a2dead032421
Massaki <enzomassaki@hotmail.com>
2022-04-22 18:07:45 -0300

    feat: adding method to export data as .xlsx

--- spacexConsumer/main/views.py
+++ spacexConsumer/main/views.py
@@ -1,7 +1,9 @@
-import csv
+import io
 from django.http import HttpResponse
 from django.shortcuts import render
+from xlsxwriter import Workbook
 import requests
+from django.core.cache import cache
 
 from .utils import most_frequent, get_json_values, get_max_dict
 
@@ -28,6 +30,47 @@ def launches(request):
         "launch_sites":result_sites,
         "launches_2019_2021":str(result_2019_2021)
         }
-
+    
+    request.session['data'] = data
 
     return render(request,"main/launches.html", {"data":data})
+
+def export_csv(request):
+    """method for exporting data into a .xlsx file."""
+    data = request.session.get('data')
+    elements = [
+        data['year_most_launches'],
+        str(data['launch_sites']),
+        data['launches_2019_2021']
+    ]
+
+    output = io.BytesIO() 
+    workbook = Workbook(output, {'in_memory': True})
+    worksheet = workbook.add_worksheet()
+
+    r = 0
+    c = 0
+
+    table_headers = [
+        'year_most_launches',
+        'launch_sites',
+        'launches_2019_2021'
+    ]
+
+    for header in table_headers:
+        worksheet.write(r, c, header)
+        c += 1
+
+    r += 1
+    c = 0
+    for element in elements:
+        worksheet.write(r, c, element)
+        c += 1
+
+    workbook.close()
+    output.seek(0)
+    content_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    response = HttpResponse(output.read(), content_type=content_type)
+    file_name = ('results')
+    response['Content-Disposition'] = "attachment; filename=" + file_name + ".xlsx"
+    return response
--- spacexConsumer/spacexConsumer/urls.py
+++ spacexConsumer/spacexConsumer/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import launches
+from main.views import launches, export_csv
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', launches, name = "launches"),
+    path('export/', export_csv)
 ]
