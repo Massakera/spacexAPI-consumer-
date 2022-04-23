85ab4e5ebd7889fa7f6ee84cc9f7e6097f12bbfe
Massaki <enzomassaki@hotmail.com>
2022-04-23 00:30:03 -0300

    refactoring: formating code

--- spacexConsumer/spacexConsumer/urls.py
+++ spacexConsumer/spacexConsumer/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from main.views import launches, export_csv
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', launches, name = "launches"),
-    path('export/', export_csv, name='export')
+    path("admin/", admin.site.urls),
+    path("", launches, name="launches"),
+    path("export/", export_csv, name="export"),
 ]
--- spacexConsumer/spacexConsumer/wsgi.py
+++ spacexConsumer/spacexConsumer/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spacexConsumer.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spacexConsumer.settings")
 
 application = get_wsgi_application()
